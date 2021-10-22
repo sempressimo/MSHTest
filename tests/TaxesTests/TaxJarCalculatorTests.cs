8ad63659a2e7f0b5b29c72bcdb714e12b96fa52b
Sempressimo <ismael.placa@golfchannel.com>
2021-10-22 10:47:53 -0400

    Splitting Tests By Class

---
+++ tests/TaxesTests/TaxJarCalculatorTests.cs
@@ -0,0 +1,46 @@
+﻿using MSHTest.Infrastructure;
+using MSHTest.Infrastructure.Services;
+using System;
+using Xunit;
+
+namespace TaxesTests
+{
+    public class TaxJarCalculatorTests
+    {
+        [Fact]
+        public void TaxJarCalculator_ShouldFailOnBadRequest()
+        {
+            TaxJarCalculator taxJarCalculator = new TaxJarCalculator();
+
+            Assert.Throws<ApplicationException>(
+                    () => taxJarCalculator.GetTaxRatesByLocation("")
+                );
+        }
+
+        [Fact]
+        public void TaxJarCalculator_ShouldReturnRatesForLocation()
+        {
+            TaxJarCalculator taxJarCalculator = new TaxJarCalculator();
+
+            var rates = taxJarCalculator.GetTaxRatesByLocation("32819");
+
+            Assert.NotNull(rates);
+
+            Assert.True(rates.State.ToUpper() == "FL");
+        }
+
+        [Fact]
+        public void TaxJarCalculator_ShouldReturnPositiveRates()
+        {
+            TaxJarCalculator taxJarCalculator = new TaxJarCalculator();
+
+            var rates = taxJarCalculator.GetTaxRatesByLocation("32819");
+
+            Assert.NotNull(rates);
+
+            Assert.True(
+                rates.CityRate >= 0 && rates.CombinedDistrictRate >= 0 && rates.CombinedRate >= 0 && rates.Country_Rate >= 0 && rates.CountyRate >= 0 && rates.StateRate >= 0
+                );
+        }
+    }
+}
--- tests/TaxesTests/TaxServiceTests.cs
+++ tests/TaxesTests/TaxServiceTests.cs
@@ -7,48 +7,6 @@ namespace TaxesTests
 {
     public class TaxesTests
     {
-        #region TaxJarCalculator
-
-        [Fact]
-        public void TaxJarCalculator_ShouldFailOnBadRequest()
-        {
-            TaxJarCalculator taxJarCalculator = new TaxJarCalculator();
-
-            Assert.Throws<ApplicationException>(
-                    ()=> taxJarCalculator.GetTaxRatesByLocation("")
-                );
-        }
-
-        [Fact]
-        public void TaxJarCalculator_ShouldReturnRatesForLocation()
-        {
-            TaxJarCalculator taxJarCalculator = new TaxJarCalculator();
-
-            var rates = taxJarCalculator.GetTaxRatesByLocation("32819");
-
-            Assert.NotNull(rates);
-
-            Assert.True(rates.State.ToUpper() == "FL"); 
-        }
-
-        [Fact]
-        public void TaxJarCalculator_ShouldReturnPositiveRates()
-        {
-            TaxJarCalculator taxJarCalculator = new TaxJarCalculator();
-
-            var rates = taxJarCalculator.GetTaxRatesByLocation("32819");
-
-            Assert.NotNull(rates);
-
-            Assert.True(
-                rates.CityRate >=0 && rates.CombinedDistrictRate >= 0 && rates.CombinedRate >= 0 && rates.Country_Rate >=0 && rates.CountyRate >= 0 && rates.StateRate >=0
-                );
-        }
-
-        #endregion
-
-        #region TaxService
-
         [Fact]
         public void TaxService_ShouldFailOnBadRequest()
         {
@@ -84,7 +42,5 @@ namespace TaxesTests
                 rates.CityRate >= 0 && rates.CombinedDistrictRate >= 0 && rates.CombinedRate >= 0 && rates.Country_Rate >= 0 && rates.CountyRate >= 0 && rates.StateRate >= 0
                 );
         }
-
-        #endregion
     }
 }
