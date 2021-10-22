4d26c3a28bac9ddad6732f4e23fcb8bf73f7c078
Sempressimo <ismael.placa@golfchannel.com>
2021-10-22 12:59:10 -0400

    Updating Tests

--- tests/TaxesTests/TaxJarCalculatorTests.cs
+++ tests/TaxesTests/TaxJarCalculatorTests.cs
@@ -1,4 +1,5 @@
-﻿using MSHTest.Infrastructure;
+﻿using MSHTest.Common.DTO;
+using MSHTest.Infrastructure;
 using MSHTest.Infrastructure.Services;
 using System;
 using Xunit;
@@ -8,12 +9,22 @@ namespace TaxesTests
     public class TaxJarCalculatorTests
     {
         [Fact]
-        public void TaxJarCalculator_ShouldFailOnBadRequest()
+        public void TaxJarCalculator_GetRatesShouldFailOnBadRequest()
         {
             TaxJarCalculator taxJarCalculator = new TaxJarCalculator();
 
             Assert.Throws<ApplicationException>(
                     () => taxJarCalculator.GetTaxRatesByLocation("")
+                );
+        }
+
+        [Fact]
+        public void TaxJarCalculator_CalculateTaxeshouldFailOnBadRequest()
+        {
+            TaxJarCalculator taxJarCalculator = new TaxJarCalculator();
+
+            Assert.Throws<ApplicationException>(
+                    () => taxJarCalculator.CalculateTaxForOrder(new TaxForOrderRequestDTO()) // Empty params
                 );
         }
 
@@ -41,6 +52,30 @@ namespace TaxesTests
             Assert.True(
                 rates.CityRate >= 0 && rates.CombinedDistrictRate >= 0 && rates.CombinedRate >= 0 && rates.Country_Rate >= 0 && rates.CountyRate >= 0 && rates.StateRate >= 0
                 );
+        }
+
+        [Fact]
+        public void TaxJarCalculator_ShouldCalculateTaxesForAnOrder()
+        {
+            TaxJarCalculator taxJarCalculator = new TaxJarCalculator();
+
+            TaxForOrderRequestDTO TaxForOrderRequest = new TaxForOrderRequestDTO()
+            {
+                Amount = 16,
+                FromCountry = "US",
+                ToCountry = "US",
+                FromState = "NJ",
+                FromZip = "07001",
+                ToZip = "07446",
+                ToState = "NJ",
+                Shipping = 1
+            };
+
+            var taxes = taxJarCalculator.CalculateTaxForOrder(TaxForOrderRequest);
+
+            Assert.NotNull(taxes);
+
+            Assert.True(taxes.TaxableAmount > 0);
         }
     }
 }
